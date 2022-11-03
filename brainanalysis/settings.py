983540884fb7c9857b9d11e73243bf5b360f97f8
Aryan Khokhar <aryankhokhar2000@gmail.com>
2022-11-03 21:28:22 +0530

    Commit

--- brainanalysis/settings.py
+++ brainanalysis/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y##)r#zqo&iqf)5efx@%_iqt+)#*xn9e_t9-*!u(2ugg)hz)hj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
