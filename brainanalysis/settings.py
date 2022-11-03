4c7aca520beb6d378662ff8438a8af34eda59a5d
Aryan Khokhar <aryankhokhar2000@gmail.com>
2022-11-03 20:51:25 +0530

    changes

--- brainanalysis/settings.py
+++ brainanalysis/settings.py
@@ -121,3 +121,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    # '/var/www/static/',
+]
