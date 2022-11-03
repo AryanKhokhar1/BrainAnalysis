6bb6494cbbc9b0117a6b9fe7d7e7e97b512eead7
Aryan Khokhar <aryankhokhar2000@gmail.com>
2022-11-03 22:40:47 +0530

    Commit

--- brainanalysis/settings.py
+++ brainanalysis/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-y##)r#zqo&iqf)5efx@%_iqt+)#*xn9e_t9-*!u(2ugg)hz)hj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','brainanalysis.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
